310e97c908e153bea62d0f7361ca671a6e527fa4
asakofujiki <a.fujiki1554@gmail.com>
2020-05-16 08:36:14 +0000

    管理者側の会員まわり

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :customers
+  end
   root 'home#top'
 
   devise_for :customers
-  devise_for :admins
+  devise_for :admins, :controllers => {
+  	:registrations => 'admins/registrations',
+  	:sessions => 'admins/sessions'
+  }
+
+  devise_scope :admin do
+  	get "sign_in", :to => "admins/sessions#new"
+  	get "sign_out", :to => "admins/sessions#destroy"
+  end
 
   resource :customers
   get 'customers/withdraw' => 'customers#withdraw'
+  get 'admin/top' => 'admin#top'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
