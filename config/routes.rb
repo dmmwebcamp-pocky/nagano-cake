9b57c8d0b5c70aa9e15b6f038e08edefb964a49b
vagrant <vagrant@localhost.localdomain>
2020-05-16 06:39:17 +0000

    [Add]admin-products

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
   resource :customers
   get 'customers/withdraw' => 'customers#withdraw'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+  	resources :products
+  end
 end
