61d468709cc45343b1b82f3c74dc3dd05c35427f
vagrant <vagrant@localhost.localdomain>
2020-05-18 06:45:32 +0000

    orderまわり追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'orders/index'
+    get 'orders/show'
+    get 'orders/update'
+  end
   root 'home#top'
 
   devise_for :customers
@@ -14,6 +19,10 @@ Rails.application.routes.draw do
 
   namespace :admin do
   	resources :genres
+  end
+
+  namespace :admin do
+    resources :orders
   end
 
 end
