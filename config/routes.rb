936ea2aa33f67c79896472665d94908eb3052652
asakofujiki <a.fujiki1554@gmail.com>
2020-05-19 11:42:58 +0000

    ヘッダー(仮)完成

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 
   devise_for :admins
 
+  get '/admin/top' => 'admin#top'
+
   get 'customer/customers/withdraw' => 'customer/customers#withdraw'
 
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
