3f5c682a1ba26f1b8cc1a56ba682a63423a23546
asakofujiki <a.fujiki1554@gmail.com>
2020-05-19 03:34:18 +0000

    管理者側会員まわりほぼ完成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,13 +8,13 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
-    #@customer = Customer.find(params[:id])
+    @customer = Customer.find(params[:id])
   end
 
   def update
     @customer = Customer.find(params[:id])
     @customer.update(customer_params)
-    redirect_to root_path
+    redirect_to admin_customer_path(@customer.id)
   end
 
   def destroy
@@ -25,6 +25,6 @@ class Admin::CustomersController < ApplicationController
 
   private
   def customer_params
-    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :email, :postal_code, :address, :phone_number, :customer_status)
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :email, :postal_code, :address, :phone_number, :customer_status, :deleted_at)
   end
 end
