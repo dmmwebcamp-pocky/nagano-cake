f2fe77de201265269671cc2e64755e1d9bde76e9
hisa008 <hisayuki.k8@gmail.com>
2020-05-24 14:47:51 +0000

    一時保存

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,10 @@ class Order < ApplicationRecord
   has_many :ordered_products, dependent: :destroy
   enum payment_method: {クレジットカード: 0,銀行振込: 1}
   enum order_status: {入金待ち: 0,入金確認: 1,製作中: 2,発送準備中: 3,発送済み: 4}
+  validates :ordered_postal_code,presence: true
+  validates :ordered_address,presence: true
+  validates :address_name,presence: true
+  validates :payment_method,presence: true
   def full_ordered
     self.ordered_postal_code + "/" + self.ordered_address + "/" + self.address_name
   end  
