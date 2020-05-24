c664e43000d6ca4fe3afd37cb4cb698dd5e82d30
vagrant <vagrant@localhost.localdomain>
2020-05-24 09:07:08 +0000

    [98%]admin-orders

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -19,7 +19,8 @@ class Admin::GenresController < ApplicationController
 		@genre = Genre.find(params[:id])
 		if @genre.update(genre_params)
 			redirect_to admin_genres_path
-		else render :edit
+		else 
+			render :edit
 		end
 	end
 
--- app/controllers/admin/ordered_products_controller.rb
+++ app/controllers/admin/ordered_products_controller.rb
@@ -1,18 +1,18 @@
 class Admin::OrderedProductsController < ApplicationController
 	def update
-  		@ordered_products = OrderedProduct.find(params[:id])
-  		@ordered_products.update(ordered_products_params)
-  		@order = Order.find(params[:id])
-  			if @ordered_products.read_attribute_before_type_cast(:production_status) == 2#2:製作中にしたら注文ステータスを2:製作中にする
+  		@ordered_product = OrderedProduct.find(params[:id])
+  		@ordered_product.update(ordered_product_params)
+  		@order = @ordered_product.order
+  			if @ordered_product.read_attribute_before_type_cast(:production_status) == 2#2:製作中にしたら注文ステータスを2:製作中にする
   				 	@order.update(order_status: 2)
-  			elsif @ordered_products.read_attribute_before_type_cast(:production_status) == 3#3:製作完了にしたら注文ステータスを3:発送準備中にする
+  			elsif @order.ordered_products.count == @order.ordered_products.where(production_status: "製作完了").count#3:製作完了にしたら注文ステータスを3:発送準備中にする
   					@order.update(order_status: 3)
-			end
-  		redirect_to request.referer
+			 end
+  		redirect_to admin_order_path(@order)
   	end
 
   	private
-	def ordered_products_params
+	def ordered_product_params
 		params.require(:ordered_product).permit(:production_status)
 	end
 end
